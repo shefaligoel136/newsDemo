bbcd52b1655ff27f12deee789f67ca6f2c037c35
shefaligoel136 <goel136shefali@gmail.com>
2021-06-10 16:49:10 +0530

    Created API and Schema for News

--- dynamodb.js
+++ dynamodb.js
@@ -3,6 +3,9 @@
 const AWS = require("aws-sdk");
 require("dotenv").config();
 
+const monotonicFactory = require("ulid").monotonicFactory;
+const ulid = monotonicFactory();
+
 AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -11,53 +14,44 @@ AWS.config.update({
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = "newsDemoAPI";
-const INDEX_NAME_1 = "city-author-index";
-const INDEX_NAME_2 = "author-index";
+const STATE_INDEX = "state-typeNews-index";
+const CITY_INDEX = "city-typeNews-index";
+const AUTHOR_INDEX = "author-typeNews-index";
 
 // CREATE
 const addNews = async (news) => {
-    const params = {
-      TableName: TABLE_NAME,
-      Item: news,
-    };
-    const newNews = await dynamoClient.put(params).promise();
-    console.log(newNews);
-    return newNews;
+  news.newsID = ulid();
+  news.timeStamp = new Date().getTime();
+  const params = {
+    TableName: TABLE_NAME, 
+    Item: news,
   };
+  const newNews = await dynamoClient.put(params).promise();
+  console.log(newNews);
+  return newNews;
+};
 
-// READ
+// READ - ALL
 const getAllNews = async () => {
   const params = {
     TableName: TABLE_NAME,
+    ScanIndexForward: true,
   };
   const news = await dynamoClient.scan(params).promise();
   console.log(news);
   return news;
 };
 
+// for LATEST NEWS -READ
 const getNewsByCity = async (city) => {
   const params = {
     TableName: TABLE_NAME,
-    IndexName: INDEX_NAME_1,
+    IndexName: CITY_INDEX,
     KeyConditionExpression: "city = :city ",
     ExpressionAttributeValues: {
       ":city": city,
     },
-  };
-  const cityWiseNews = await dynamoClient.query(params).promise();
-  console.log(cityWiseNews);
-  return cityWiseNews;
-};
-
-const getNewsByCityAuthor = async (city, author) => {
-  const params = {
-    TableName: TABLE_NAME,
-    IndexName: INDEX_NAME_1,
-    KeyConditionExpression: "city = :city and author = :author",
-    ExpressionAttributeValues: {
-      ":city": city,
-      ":author": author,
-    },
+    ScanIndexForward: false,
   };
   const cityWiseNews = await dynamoClient.query(params).promise();
   console.log(cityWiseNews);
@@ -65,26 +59,155 @@ const getNewsByCityAuthor = async (city, author) => {
 };
 
 const getNewsByAuthor = async (author) => {
-    const params = {
-      TableName: TABLE_NAME,
-      IndexName: INDEX_NAME_2,
-      KeyConditionExpression: "author = :author",
-      ExpressionAttributeValues: {
-        ":author": author,
-      },
-    };
-    const authorWiseNews = await dynamoClient.query(params).promise();
-    console.log(authorWiseNews);
-    return authorWiseNews;
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: AUTHOR_INDEX,
+    KeyConditionExpression: "author = :author",
+    ExpressionAttributeValues: {
+      ":author": author,
+    },
+    ScanIndexForward: false,
   };
+  const authorWiseNews = await dynamoClient.query(params).promise();
+  console.log(authorWiseNews);
+  return authorWiseNews;
+};
+
+const getNewsByState = async (state) => {
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: STATE_INDEX,
+    KeyConditionExpression: "state = :state",
+    ExpressionAttributeValues: {
+      ":state": state,
+    },
+    ScanIndexForward: false,
+  };
+  const stateWiseNews = await dynamoClient.query(params).promise();
+  console.log(stateWiseNews);
+  return stateWiseNews;
+};
+
+// FOR TRENDING NEWS - READ
+
+const getNewsByStateHighlight = async (state) => {
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: STATE_INDEX,
+    KeyConditionExpression: "#state = :state",
+    FilterExpression: 'highlight >= :val',
+    ExpressionAttributeNames: {
+      "#state": "state",
+      
+    },
+    ExpressionAttributeValues: {
+      ":state": state,
+      ":val": 1
+    },
+    ScanIndexForward: false,
+  };
+  const stateWiseHighlight = await dynamoClient.query(params).promise();
+  console.log(stateWiseHighlight);
+  return stateWiseHighlight;
+};
+
+const getNewsByCityHighlight = async (city) => {
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: CITY_INDEX,
+    KeyConditionExpression: "city = :city",
+    FilterExpression: 'highlight >= :val',
+    ExpressionAttributeValues: {
+      ":city": city,
+      ":val": 1
+    },
+    ScanIndexForward: false,
+  };
+  const cityWiseHighlight = await dynamoClient.query(params).promise();
+  console.log(cityWiseHighlight);
+  return cityWiseHighlight;
+};
+
+const getNewsByAuthorHighlight = async (author) => {
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: AUTHOR_INDEX,
+    KeyConditionExpression: "author = :author",
+    FilterExpression: 'highlight >= :val',
+    ExpressionAttributeValues: {
+      ":author": author,
+      ":val": 1
+    },
+    ScanIndexForward: false,
+  };
+  const authorWiseHighlight = await dynamoClient.query(params).promise();
+  console.log(authorWiseHighlight);
+  return authorWiseHighlight;
+};
+
+// FOR TYPE NEWS - READ
+const getNewsByStatetypeNews = async (state, typeNews) => {
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: STATE_INDEX,
+    KeyConditionExpression:
+      "#state = :state  and begins_with(typeNews, :typeNews)",
+    ExpressionAttributeNames: {
+      "#state": "state",
+    },
+    ExpressionAttributeValues: {
+      ":state": state,
+      ":typeNews": typeNews,
+    },
+    ScanIndexForward: false,
+  };
+  const stateWiseTypeNews = await dynamoClient.query(params).promise();
+  console.log(stateWiseTypeNews);
+  return stateWiseTypeNews;
+};
+
+const getNewsByCitytypeNews = async (city, typeNews) => {
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: CITY_INDEX,
+    KeyConditionExpression:
+      "city = :city  and begins_with(typeNews, :typeNews)",
+    ExpressionAttributeValues: {
+      ":city": city,
+      ":typeNews": typeNews,
+    },
+    ScanIndexForward: false,
+  };
+  const cityWiseTypeNews = await dynamoClient.query(params).promise();
+  console.log(cityWiseTypeNews);
+  return cityWiseTypeNews;
+};
+
+const getNewsByAuthortypeNews = async (author, typeNews) => {
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: AUTHOR_INDEX,
+    KeyConditionExpression:
+      "author = :author  and begins_with(typeNews, :typeNews)",
+    ExpressionAttributeValues: {
+      ":author": author,
+      ":typeNews": typeNews,
+    },
+    ScanIndexForward: false,
+  };
+  console.log(author);
+  const authorWiseTypeNews = await dynamoClient.query(params).promise();
+  console.log(authorWiseTypeNews);
+  return authorWiseTypeNews;
+};
 
 // UPDATE
 
-const updateNewsTitle = async (id,title) => {
+const updateNewsTitle = async (newsID, title) => {
   const params = {
     TableName: TABLE_NAME,
     Key: {
-        id,
+      newsID,
     },
     UpdateExpression: "SET title = :title",
     ExpressionAttributeValues: {
@@ -96,44 +219,64 @@ const updateNewsTitle = async (id,title) => {
   return updatedNewsTitle;
 };
 
-const updateNewsDescription = async (id,description) => {
-    const params = {
-      TableName: TABLE_NAME,
-      Key: {
-          id,
-      },
-      UpdateExpression: "SET description = :description",
-      ExpressionAttributeValues: {
-        ":description": description,
-      },
-    };
-    const updateNewsDescription = await dynamoClient.update(params).promise();
-    console.log(updateNewsDescription);
-    return updateNewsDescription;
+const updateNewsDescription = async (newsID, description) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      newsID,
+    },
+    UpdateExpression: "SET description = :description",
+    ExpressionAttributeValues: {
+      ":description": description,
+    },
   };
-
-
-// DELETE
-const deleteNews = async (id) => {
-    const params = {
-      TableName: TABLE_NAME,
-      Key: {
-          id,
-      },
-    }
-    return await dynamoClient.delete(params).promise();  
+  const updateNewsDescription = await dynamoClient.update(params).promise();
+  console.log(updateNewsDescription);
+  return updateNewsDescription;
 };
 
+const updatedNewsHighlight = async (newsID) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      newsID,
+    },
+    UpdateExpression: "SET highlight = highlight + :val",
+    ExpressionAttributeValues: {
+      ":val": 1
+    },
+  };
+  const updatedNewsHighlight = await dynamoClient.update(params).promise();
+  console.log(updatedNewsHighlight);
+  return updatedNewsHighlight;
+}
 
+// DELETE
+const deleteNews = async (newsID) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      newsID,
+    },
+  };
+  return await dynamoClient.delete(params).promise();
+};
 
 module.exports = {
-    dynamoClient,
-    addNews,
-    getAllNews,
-    getNewsByAuthor,
-    getNewsByCity,
-    getNewsByCityAuthor,
-    updateNewsDescription,
-    updateNewsTitle,
-    deleteNews
-}
+  dynamoClient,
+  addNews,
+  getAllNews,
+  getNewsByAuthor,
+  getNewsByCity,
+  getNewsByState,
+  getNewsByStatetypeNews,
+  getNewsByCitytypeNews,
+  getNewsByAuthortypeNews,
+  getNewsByStateHighlight,
+  getNewsByCityHighlight,
+  getNewsByAuthorHighlight,
+  updateNewsDescription,
+  updateNewsTitle,
+  updatedNewsHighlight,
+  deleteNews,
+};
--- index.js
+++ index.js
@@ -6,14 +6,32 @@ const {
   getAllNews,
   getNewsByAuthor,
   getNewsByCity,
-  getNewsByCityAuthor,
+  getNewsByState,
+  getNewsByStatetypeNews,
+  getNewsByCitytypeNews,
+  getNewsByAuthortypeNews,
+  getNewsByStateHighlight,
+  getNewsByCityHighlight,
+  getNewsByAuthorHighlight,
   updateNewsDescription,
   updateNewsTitle,
+  updatedNewsHighlight,
   deleteNews,
 } = require("./dynamodb");
 
 app.use(express.json());
 
+//CREATE
+app.post('/addNews',async(req,res) => {
+  const news = req.body;
+  try{
+    const add = await addNews(news);
+    res.json(add);
+  }catch(error){
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+})
 
 // READ
 app.get("/", (req, res) => {
@@ -31,78 +49,174 @@ app.get("/news", async (req, res) => {
 });
 
 app.get("/newsCity/:city", async (req, res) => {
-    console.log("city", req.params);
-    const { city } = req.params;
-    try {
-      const newsByCity = await getNewsByCity(city);
-      res.json(newsByCity);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ err: "Something went wrong" });
-    }
-  });
+  console.log("city", req.params);
+  const { city } = req.params;
+  try {
+    const newsByCity = await getNewsByCity(city);
+    res.json(newsByCity);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 
 app.get("/newsAuthor/:author", async (req, res) => {
-    const {author} = req.params
-    try {
-      const newsByAuthor = await getNewsByAuthor(author);
-      res.json(newsByAuthor);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ err: "Something went wrong" });
-    }
-  });
+  const { author } = req.params;
+  try {
+    const newsByAuthor = await getNewsByAuthor(author);
+    res.json(newsByAuthor);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 
-  app.get("/newsCityAuthor/:city/:author", async (req, res) => {
-    
-    try {
-      const newsCityAuthor = await getNewsByCityAuthor(req.params.city,req.params.author);
-      res.json(newsCityAuthor);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ err: "Something went wrong" });
-    }
-  });
+app.get("/newsState/:state", async (req, res) => {
+  const { state } = req.params;
+  try {
+    const newsByState = await getNewsByState(state);
+    res.json(newsByState);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 
+app.get("/newsStateType/:state/:typeNews", async (req, res) => {
+  try {
+    const newsByState = await getNewsByStatetypeNews(
+      req.params.state,
+      req.params.typeNews
+    );
+    res.json(newsByState);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.get("/newsCityType/:city/:typeNews", async (req, res) => {
+  try {
+    const newsByCity = await getNewsByCitytypeNews(
+      req.params.city,
+      req.params.typeNews
+    );
+    res.json(newsByCity);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.get("/newsAuthorType/:author/:typeNews", async (req, res) => {
+  try {
+    const newsByAuthor = await getNewsByAuthortypeNews(
+      req.params.author,
+      req.params.typeNews
+    );
+    res.json(newsByAuthor);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.get("/newsCityHighlight/:city", async (req, res) => {
+  const { city } = req.params;
+  try {
+    const newsByCity = await getNewsByCityHighlight(city);
+    res.json(newsByCity);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.get("/newsStateHighlight/:state", async (req, res) => {
+  const { state } = req.params;
+  try {
+    const newsByState = await getNewsByStateHighlight(state);
+    res.json(newsByState);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.get("/newsAuthorHighlight/:author", async (req, res) => {
+  const { author } = req.params;
+  try {
+    const newsByAuthor = await getNewsByAuthorHighlight(author);
+    res.json(newsByAuthor);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 
 //   UPDATE
-  app.put("/newsUpdateTitle/:id/:title", async (req, res) => {
-    
-    try {
-      const newsUpdate = await updateNewsTitle(req.params.id,req.params.title);
-      res.json(newsUpdate);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ err: "Something went wrong" });
-    }
-  });
+app.put("/newsUpdateTitle/:newsID/:title", async (req, res) => {
+  try {
+    const newsUpdate = await updateNewsTitle(req.params.newsID, req.params.title);
+    res.json(newsUpdate);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 
-  app.put("/newsUpdateDescription/:id/:description", async (req, res) => {
-    
-    try {
-      const newsUpdate = await updateNewsDescription(req.params.id,req.params.description);
-      res.json(newsUpdate);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ err: "Something went wrong" });
-    }
-  });
+app.put("/newsUpdateDescription/:newsID/:description", async (req, res) => {
+  try {
+    const newsUpdate = await updateNewsDescription(
+      req.params.newsID,
+      req.params.description
+    );
+    res.json(newsUpdate);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.put("/newsUpdateHighlight/:newsID", async (req, res) => {
+  try {
+    const newsUpdate = await updatedNewsHighlight(req.params.newsID);
+    res.json(newsUpdate);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 
 //   DELETE
-  app.delete("/newsDelete/:id", async (req, res) => {
-    
-    try {
-      const newsUpdate = await deleteNews(req.params.id);
-      res.json(newsUpdate);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ err: "Something went wrong" });
-    }
-  });
-
+app.delete("/newsDelete/:newsID", async (req, res) => {
+  try {
+    const newsUpdate = await deleteNews(req.params.newsID);
+    res.json(newsUpdate);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 
 const port = process.env.PORT || 8800;
 
 app.listen(port, () => {
   console.log(`Running on port - ${port}`);
 });
+
+
+// {
+//   "author": "Shefali",
+//   "city": "Kanpur",
+//   "description": "Covid news",
+//   "highlight": 0,
+//   "newsID": "",
+//   "reportReason": [
+//       "NA"
+//   ],
+//   "state": "UP",
+//   "timeStamp": "",
+//   "title": "Elections",
+//   "typeNews": "GEN"
+// }
